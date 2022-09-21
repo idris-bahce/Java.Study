796b942f8d52a0b8dd1a2d39f474be259b4f0b1c
idris <idrisbahce26@gmail.com>
2022-09-21 06:51:52 +0300

    go on working

--- src/Excercises/Exc5_08.java
+++ src/Excercises/Exc5_08.java
@@ -7,28 +7,33 @@ public class Exc5_08 {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of the students:");
         int numOfStudents = in.nextInt();
-        int counter = 1;
-        System.out.println("Enter the students name and their mark:");
+        System.out.println("Enter each student and thei scores(i.e. James 45):");
         String name = in.next();
-        int mark = in.nextInt();
-        int first= mark;
-        int second = 0;
-        String name2 = "";
-        String name1 = name;
-        while (counter<numOfStudents) {
-            name2 = in.next();
-            second = in.nextInt();
-            if(first<second){
-                first = second;
-                name1 = name2;
-            }else if (second<first){
-                second = first;
-                name2=name1;
-            }
+        int score = in.nextInt();
+        int counter = 1;
+        String str2 = "";
+        int s2 = 0;
+
+
+        while (counter < numOfStudents) {
+            String str1 = in.next();
+            int s1 = in.nextInt();
             counter++;
+
+            if (s1 > score) {
+                str2 = name;
+                s2 = score;
+                score = s1;
+                str1 = name;
+            } else if (s2 < s1) {
+                str2 = str1;
+                s2 = s1;
+            }
         }
-        System.out.println(name1 + " has the highest mark: " + first);
-        System.out.println(name2 + " has the second highest mark: " + second);
-        }
+
+        System.out.println("The highest score is " + name + "'s score, it is " + score);
+        System.out.println("The highest score is " + str2 + "'s score, it is " + s2);
+
     }
+}
 
